c2e646a9cb942afd9aff1aab6411e141ef34e88d
Abigail2507 <aby2507@gmail.com>
2024-02-08 17:15:51 -0600

    ejercicio1

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// Escribe tu código aquí.
+function bigWords(inputWord, wordArray) {
+    return wordArray.filter(word => word.length > inputWord.length);
+}
+
+
+function printArray(wordArray) {
+    const list = document.getElementById("outputList");
+    list.innerHTML = ""; 
+    wordArray.forEach(word => {
+        const listItem = document.createElement("li");
+        listItem.textContent = word;
+        list.appendChild(listItem);
+    });
+}
+
+
+function processInput() {
+    const inputWord = document.getElementById("inputWord").value.trim();
+    const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio']; 
+    printArray(filteredArray);
+}
